94b72bc3c24894f70edfc1e82bbf4a3774027b2d
azzamsa <oktinastuzi@gmail.com>
2018-05-19 17:58:16 +0700

    put a comment for readability

--- take-your-moments/take-your-moments.sh
+++ take-your-moments/take-your-moments.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# take your moments
+# take screnshot and give it a name directly
+
 img_name=$(yad \
 --width=200 \
 --form \
@@ -7,9 +10,5 @@ img_name=$(yad \
 --button="Save:1" \
 --center)
 
-# cd ~/Pictures/screenshot/
-# mv $(date +%F-%H:%M)*.png ~/moments/${img_name:0:-1}.png
+# using bash subtring to remove '|' character from yad input
 exec maim $HOME/moments/${img_name:0:-1}_$(date +%F-%H:%M).png
-# testing
-# echo ${img_name:0:-1}
-# echo ${img_name:0:-1}_$(date +%F-%H:%M)
