57554ab3edb42920a737c152bdd29e016f2fb42f
azzamsa <azzam@azzamsa.com>
2019-05-28 16:06:14 +0700

    Add ability to choose between full and selection

--- take-your-moments/take-your-moments.sh
+++ take-your-moments/take-your-moments.sh
@@ -2,14 +2,19 @@
 
 # take your moments
 # take screnshot and give it a name directly
-
 img_name=$(yad \
---width=200 \
---form \
---field="Name" \
---button="Save" \
---center)
+               --width=200 \
+               --form \
+               --field="Name" \
+               --button="Save" \
+               --center)
 
-# using bash subtring to remove '|' character from yad input
-exec maim $HOME/Pictures/moments/"${img_name:0:-1}_$(date +%F-%H:%M).png" &
-exec echo $HOME/Pictures/moments/"${img_name:0:-1}_$(date +%F-%H:%M).png" | xclip -selection clipboard
+case "$1" in
+    full)
+        # using bash subtring to remove '|' character from yad input
+        exec maim --hidecursor $HOME/Pictures/moments/"${img_name:0:-1}_$(date +%F-%H:%M).png"
+        ;;
+    selection)
+        exec maim -s --hidecursor $HOME/Pictures/moments/"${img_name:0:-1}_$(date +%F-%H:%M).png"
+        ;;
+esac
